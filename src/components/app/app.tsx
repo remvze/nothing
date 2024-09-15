c952123d9b6f6b7569472102de1765e0a63df7da
MAZE <remvze@proton.me>
2024-09-15 23:09:13 +0330

    feat: change copy

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -90,33 +90,48 @@ function AppComponent() {
 
       <div className={styles.divider} />
 
-      <p className={styles.desc}>
-        Nothing. A timer that tracks your choice to do... nothing. No goals to
-        chase, no notifications to grab your attention, no pressure to fill the
-        silence with productivity. It simply exists, quietly counting each
-        second you let slip by. This is a space where inactivity is the point—a
-        digital oasis amidst the chaos of endless tasks and to-dos. Stay as long
-        as you like, watch the seconds tick away, or let your gaze wander
-        elsewhere. There’s no reward for sticking around, just the peculiar
-        pleasure of existing in the void. Because sometimes, the most profound
-        act is to do absolutely nothing at all.
-        <br />
-        <br />
-        Created by{' '}
-        <a href="https://x.com/remvze" rel="noreferrer" target="_blank">
-          Maze
-        </a>
-        .<br />
-        Code available on{' '}
-        <a
-          href="https://github.com/remvze/nothing"
-          rel="noreferrer"
-          target="_blank"
-        >
-          GitHub
-        </a>
-        .
-      </p>
+      <div className={styles.desc}>
+        <p>
+          Nothing—a timer that tracks your intentional choice to do... nothing.
+          No goals to chase, no notifications clamoring for your attention, no
+          pressure to fill the silence with productivity. It simply exists,
+          quietly counting each second you allow to pass. This is a space where
+          inactivity is the point—a digital oasis amidst the chaos of endless
+          tasks and to-dos.
+        </p>
+        <p>
+          Yet the essence of Nothing isn&apos;t about fixating on your screen,
+          though you&apos;re welcome to if you wish. It&apos;s about setting
+          aside your phone or computer, taking a step back from the relentless
+          grind, and reconnecting with the world around you. Nothing is more a
+          concept than an app—a quiet reflection rather than a tool to be used.
+          It stands as a gentle rebellion against the incessant noise of modern
+          life, which demands constant action.
+        </p>
+        <p>
+          Stay as long as you like, watch the seconds tick away, or let your
+          gaze wander elsewhere. There&apos;s no reward for lingering, just the
+          peculiar pleasure of simply being. Sometimes, the most profound act is
+          to pause, breathe, and do nothing at all—a reminder that it&apos;s
+          perfectly acceptable to embrace stillness and just... be.
+        </p>
+        <p className={styles.noIndent}>
+          Created by{' '}
+          <a href="https://x.com/remvze" rel="noreferrer" target="_blank">
+            Maze
+          </a>
+          .<br />
+          Code available on{' '}
+          <a
+            href="https://github.com/remvze/nothing"
+            rel="noreferrer"
+            target="_blank"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+      </div>
     </Container>
   );
 }
